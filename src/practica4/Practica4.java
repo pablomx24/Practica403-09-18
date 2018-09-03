1c11441d0bb982d9d22c8d1cbae7044dab6bb25e
Brian Alejandro Castañeda Martinez <alex029610@gmail.com>
2018-09-03 18:01:56 -0500

    update project4

--- src/practica4/Practica4.java
+++ src/practica4/Practica4.java
@@ -15,7 +15,12 @@ public class Practica4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       saludoteam();
+        
+    }
+    public static void saludoteam(){
+    
+     System.out.println("Hello team !!!");
     }
     
 }
