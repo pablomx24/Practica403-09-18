47c3220f7e8c68d80dd46548518014a55b7973ab
Pablo Antonio Macias Cervantes <pablomacias_24@hotmail.com>
2018-09-03 23:30:00 -0500

    Listo

--- src/practica4/Practica4.java
+++ src/practica4/Practica4.java
@@ -17,46 +17,10 @@ public class Practica4 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       saludoteam();
-       Update1PM();
-       Update1CAHG();
-       Update2CAHG(); 
-       sumaCarlos();
+       Adios();
     }
-    public static void saludoteam(){
-    
-     System.out.println("No pudo El Brian !!!");
-    }
-    public static void Update1PM(){
-        System.out.println("va el primer Update");
-        System.out.println("Esta es mi Segunda UpdatePM");
-    }
-    
-    public static void Update1CAHG(){
-        System.out.println("Actualizado");
-    }
-    
-    public static void Update2CAHG(){
-        System.out.println("Segunda Actualizacion");
-    }
-    
-    public static void sumaCarlos(){
-        
-        int x = 0;
-        int y = 0;
-        
-        System.out.print("Ingrese un numero: \n>> ");
-        Scanner dato1 = new Scanner (System.in);
-        x = dato1.nextInt();
-        
-        System.out.print("Ingrese otro numero: \n>> ");
-        Scanner dato2 = new Scanner (System.in);
-        y = dato2.nextInt();
-        
-        int resultado = x + y;
-        
-        
-        System.out.println("El resultado de la suma es: \n" + resultado);
+    public static void Adios(){
+        System.out.println("TODO SE FUE");
     }
     
 }
